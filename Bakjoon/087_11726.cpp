c50482778aeebce8d68952b4e4de17d3ad68085b
ldcity <61225455+ldcity@users.noreply.github.com>
2023-11-26 17:38:06 +0900

    Add files via upload

---
+++ Bakjoon/087_11726.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<long long> D;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	D.resize(n + 1);
+
+	D[1] = 1;
+	D[2] = 2;
+
+	for (int i = 3; i <= n; i++)
+	{
+		D[i] = D[i - 2] + D[i - 1];
+	}
+
+	cout << D[n] << endl;
+
+	return 0;
+}
+
+
